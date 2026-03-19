538a3817ffc8e9d52a1cd92e7d57d889fbe81208
ilalonde1 <ian@oiloftrop.ca>
2026-03-18 18:46:32 -0700

    fix: resolve XamlParseException in BrochureContentStep
    - Replaced invalid DataTrigger Value={Binding} with MultiBinding approach
    - Added ReferenceEqualsMultiConverter for selected project row highlight
    - DataTrigger.Value now correctly uses boolean True/False

--- Kor.Operations.App/GeneralTools/BrochureContentStep.xaml.cs
+++ Kor.Operations.App/GeneralTools/BrochureContentStep.xaml.cs
@@ -415,4 +415,18 @@ namespace Kor.Operations.GeneralTools
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
             => throw new NotSupportedException();
     }
+
+    public sealed class ReferenceEqualsMultiConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length < 2)
+                return false;
+
+            return ReferenceEquals(values[0], values[1]);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+            => throw new NotSupportedException();
+    }
 }
