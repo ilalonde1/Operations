8ec6a1523081ca0458d92961d6d765fe32890bba
Ian Lalonde <ilalonde@korstructural.com>
2026-05-05 15:49:30 -0700

    fix(financials): apply Billed P&L view to embedded GlProfitLossView too
    41196f1 added the Billed/Posted/Side-by-side toggle to the standalone GlProfitLossWindow but missed the GlProfitLossView UserControl embedded in the main FinancialsWindow's P&L Report tab. The tab continued to show GLSummary-only revenue (~ YTD vs the ~.29M actually billed). Mirrors the same toggle, reconciliation banner, and BilledFinancialsPresenter wiring into the View. Drilldown code (PnLGrid_MouseDoubleClick et al) is preserved verbatim.

--- Kor.Operations.App/Financials/GlProfitLossView.xaml.cs
+++ Kor.Operations.App/Financials/GlProfitLossView.xaml.cs
@@ -12,46 +12,216 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using Kor.Operations.App.Options;
-using Microsoft.Extensions.DependencyInjection;
+using Kor.Operations.Services;
 using Serilog;
 
 namespace Kor.Operations.Financials
 {
     public partial class GlProfitLossView : UserControl
     {
+        private readonly BilledFinancialsPresenter _billedPresenter;
         private readonly GlProfitLossPresenter _presenter;
+        private readonly GlProfitLossPresenter _sideBySidePostedPresenter;
+        private bool _initialized;
+        private PnlViewMode _currentMode = PnlViewMode.Billed;
 
         public GlProfitLossView()
             : this(
                 Kor.Operations.Services.AppServices.Get<GlProfitLossService>(),
+                Kor.Operations.Services.AppServices.Get<BilledFinancialsService>(),
                 Kor.Operations.Services.AppServices.Get<FinancialsOptions>())
         {
         }
 
-        public GlProfitLossView(GlProfitLossService glProfitLossService, FinancialsOptions financialsOptions)
+        public GlProfitLossView(
+            GlProfitLossService glProfitLossService,
+            BilledFinancialsService billedFinancialsService,
+            FinancialsOptions financialsOptions)
         {
             InitializeComponent();
-            _presenter = new GlProfitLossPresenter(
-                this,
-                glProfitLossService ?? throw new ArgumentNullException(nameof(glProfitLossService)),
-                financialsOptions ?? throw new ArgumentNullException(nameof(financialsOptions)),
+            var options = financialsOptions ?? throw new ArgumentNullException(nameof(financialsOptions));
+
+            _billedPresenter = new BilledFinancialsPresenter(
+                billedFinancialsService ?? throw new ArgumentNullException(nameof(billedFinancialsService)),
+                options,
                 PnLGrid,
                 NetTrendCanvas,
                 NetTrendLine,
                 RevExpCanvas,
                 NetTrendLabelGrid,
                 RevExpLabelGrid);
-            DataContext = _presenter.ViewModel;
+
+            _presenter = new GlProfitLossPresenter(
+                this,
+                glProfitLossService ?? throw new ArgumentNullException(nameof(glProfitLossService)),
+                options,
+                PnLGrid,
+                NetTrendCanvas,
+                NetTrendLine,
+                RevExpCanvas,
+                NetTrendLabelGrid,
+                RevExpLabelGrid);
+
+            _sideBySidePostedPresenter = new GlProfitLossPresenter(
+                this,
+                glProfitLossService,
+                options,
+                PostedPnLGrid,
+                NetTrendCanvas,
+                NetTrendLine,
+                RevExpCanvas,
+                NetTrendLabelGrid,
+                RevExpLabelGrid);
+
+            DataContext = _billedPresenter.ViewModel;
+
+            AppServices.GetOptional<AppAiContextBuilder>()?.Register(_billedPresenter.ViewModel);
+            AppServices.GetOptional<AppAiContextBuilder>()?.Register(_presenter.ViewModel);
+            Unloaded += (_, _) =>
+            {
+                var builder = AppServices.GetOptional<AppAiContextBuilder>();
+                builder?.Unregister(_billedPresenter.ViewModel);
+                builder?.Unregister(_presenter.ViewModel);
+            };
         }
+
+        private bool IsPostedMode => PostedViewRadio.IsChecked == true;
+
+        private bool IsSideBySideMode => SideBySideViewRadio.IsChecked == true;
 
         private async void Window_Loaded(object sender, RoutedEventArgs e)
         {
             await _presenter.InitializeAsync().ConfigureAwait(true);
+            await _sideBySidePostedPresenter.InitializeAsync().ConfigureAwait(true);
+            await _billedPresenter.InitializeAsync().ConfigureAwait(true);
+            _initialized = true;
+            BilledViewRadio.IsChecked = true;
+            _currentMode = PnlViewMode.Billed;
+            ApplyViewMode();
+        }
+
+        private async void ViewMode_Checked(object sender, RoutedEventArgs e)
+        {
+            if (!_initialized)
+                return;
+
+            var nextMode = GetSelectedMode();
+            CopyFilters(_currentMode, nextMode);
+            _currentMode = nextMode;
+            ApplyViewMode();
+            await RefreshActiveAsync(forceRefresh: false).ConfigureAwait(true);
+        }
+
+        private void ApplyViewMode()
+        {
+            if (IsPostedMode)
+            {
+                DataContext = _presenter.ViewModel;
+                PnLGrid.Visibility = Visibility.Visible;
+                PostedPnLGrid.Visibility = Visibility.Collapsed;
+                PostedGridLabel.Visibility = Visibility.Collapsed;
+                TableFilterPanel.Visibility = Visibility.Visible;
+                FlipSignCheckBox.Visibility = Visibility.Visible;
+                ReconciliationPanel.Visibility = Visibility.Collapsed;
+                PostedLagPanel.Visibility = _presenter.ViewModel.PostingLagVisibility;
+                PnLGrid.MouseDoubleClick -= PnLGrid_MouseDoubleClick;
+                PnLGrid.MouseDoubleClick += PnLGrid_MouseDoubleClick;
+                _presenter.RenderCharts();
+                return;
+            }
+
+            DataContext = _billedPresenter.ViewModel;
+            PnLGrid.Visibility = Visibility.Visible;
+            PostedPnLGrid.Visibility = IsSideBySideMode ? Visibility.Visible : Visibility.Collapsed;
+            PostedGridLabel.Visibility = IsSideBySideMode ? Visibility.Visible : Visibility.Collapsed;
+            TableFilterPanel.Visibility = Visibility.Collapsed;
+            FlipSignCheckBox.Visibility = Visibility.Collapsed;
+            ReconciliationPanel.Visibility = _billedPresenter.ViewModel.ReconciliationVisibility;
+            PostedLagPanel.Visibility = Visibility.Collapsed;
+            PnLGrid.MouseDoubleClick -= PnLGrid_MouseDoubleClick;
+            _billedPresenter.RenderCharts();
+        }
+
+        private void SyncFiltersFromActive()
+        {
+            if (IsPostedMode)
+            {
+                _billedPresenter.ViewModel.FromDate = _presenter.ViewModel.FromDate;
+                _billedPresenter.ViewModel.ToDate = _presenter.ViewModel.ToDate;
+                _billedPresenter.ViewModel.OrgFilter = _presenter.ViewModel.OrgFilter;
+                _billedPresenter.ViewModel.HideZeroRows = _presenter.ViewModel.HideZeroRows;
+                _sideBySidePostedPresenter.ViewModel.FromDate = _presenter.ViewModel.FromDate;
+                _sideBySidePostedPresenter.ViewModel.ToDate = _presenter.ViewModel.ToDate;
+                _sideBySidePostedPresenter.ViewModel.OrgFilter = _presenter.ViewModel.OrgFilter;
+                _sideBySidePostedPresenter.ViewModel.HideZeroRows = _presenter.ViewModel.HideZeroRows;
+                return;
+            }
+
+            _presenter.ViewModel.FromDate = _billedPresenter.ViewModel.FromDate;
+            _presenter.ViewModel.ToDate = _billedPresenter.ViewModel.ToDate;
+            _presenter.ViewModel.OrgFilter = _billedPresenter.ViewModel.OrgFilter;
+            _presenter.ViewModel.HideZeroRows = _billedPresenter.ViewModel.HideZeroRows;
+            _sideBySidePostedPresenter.ViewModel.FromDate = _billedPresenter.ViewModel.FromDate;
+            _sideBySidePostedPresenter.ViewModel.ToDate = _billedPresenter.ViewModel.ToDate;
+            _sideBySidePostedPresenter.ViewModel.OrgFilter = _billedPresenter.ViewModel.OrgFilter;
+            _sideBySidePostedPresenter.ViewModel.HideZeroRows = _billedPresenter.ViewModel.HideZeroRows;
+        }
+
+        private void CopyFilters(PnlViewMode fromMode, PnlViewMode toMode)
+        {
+            if (fromMode == toMode)
+                return;
+
+            if (fromMode == PnlViewMode.Posted && toMode != PnlViewMode.Posted)
+            {
+                _billedPresenter.ViewModel.FromDate = _presenter.ViewModel.FromDate;
+                _billedPresenter.ViewModel.ToDate = _presenter.ViewModel.ToDate;
+                _billedPresenter.ViewModel.OrgFilter = _presenter.ViewModel.OrgFilter;
+                _billedPresenter.ViewModel.HideZeroRows = _presenter.ViewModel.HideZeroRows;
+                return;
+            }
+
+            if (fromMode != PnlViewMode.Posted && toMode == PnlViewMode.Posted)
+            {
+                _presenter.ViewModel.FromDate = _billedPresenter.ViewModel.FromDate;
+                _presenter.ViewModel.ToDate = _billedPresenter.ViewModel.ToDate;
+                _presenter.ViewModel.OrgFilter = _billedPresenter.ViewModel.OrgFilter;
+                _presenter.ViewModel.HideZeroRows = _billedPresenter.ViewModel.HideZeroRows;
+            }
+        }
+
+        private PnlViewMode GetSelectedMode()
+        {
+            if (PostedViewRadio.IsChecked == true)
+                return PnlViewMode.Posted;
+            if (SideBySideViewRadio.IsChecked == true)
+                return PnlViewMode.SideBySide;
+            return PnlViewMode.Billed;
+        }
+
+        private async Task RefreshActiveAsync(bool forceRefresh)
+        {
+            SyncFiltersFromActive();
+            if (IsPostedMode)
+            {
+                await _presenter.RefreshAsync(forceRefresh).ConfigureAwait(true);
+            }
+            else if (IsSideBySideMode)
+            {
+                await _billedPresenter.RefreshAsync(forceRefresh).ConfigureAwait(true);
+                await _sideBySidePostedPresenter.RefreshAsync(forceRefresh).ConfigureAwait(true);
+            }
+            else
+            {
+                await _billedPresenter.RefreshAsync(forceRefresh).ConfigureAwait(true);
+            }
+
+            ApplyViewMode();
         }
 
         private async void RefreshBtn_Click(object sender, RoutedEventArgs e)
         {
-            await _presenter.RefreshAsync(forceRefresh: true).ConfigureAwait(true);
+            await RefreshActiveAsync(forceRefresh: true).ConfigureAwait(true);
         }
 
         private void MetricDictionaryBtn_Click(object sender, RoutedEventArgs e)
@@ -67,13 +237,43 @@ namespace Kor.Operations.Financials
             }
         }
 
-        private void NetTrendCanvas_SizeChanged(object sender, SizeChangedEventArgs e) => _presenter.RenderCharts();
+        private void NetTrendCanvas_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (IsPostedMode)
+                _presenter.RenderCharts();
+            else
+                _billedPresenter.RenderCharts();
+        }
 
-        private void RevExpCanvas_SizeChanged(object sender, SizeChangedEventArgs e) => _presenter.RenderCharts();
+        private void RevExpCanvas_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (IsPostedMode)
+                _presenter.RenderCharts();
+            else
+                _billedPresenter.RenderCharts();
+        }
 
         private async void ExportBtn_Click(object sender, RoutedEventArgs e)
         {
-            await _presenter.ExportAsync(Window.GetWindow(this)).ConfigureAwait(true);
+            if (IsPostedMode || IsSideBySideMode)
+            {
+                await _presenter.ExportAsync(Window.GetWindow(this)).ConfigureAwait(true);
+                return;
+            }
+
+            MessageBox.Show(
+                Window.GetWindow(this) ?? Application.Current?.MainWindow,
+                "Billed P&L export is not wired yet. Use the grid view or switch to Posted (GL) for Excel export.",
+                "Export to Excel",
+                MessageBoxButton.OK,
+                MessageBoxImage.Information);
+        }
+
+        private enum PnlViewMode
+        {
+            Billed,
+            Posted,
+            SideBySide
         }
 
         private void PnLGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
